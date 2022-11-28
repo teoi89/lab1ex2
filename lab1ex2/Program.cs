1442f6d7e284e7135c285418ebb0db4b5285eaf5
teoi89 <teofil.lucaci@yahoo.ro>
2022-11-28 16:03:09 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va calcula media aritmetica a trei numere citite de la
+            tastatura */
+
+            Console.WriteLine("This program will calculate arithmetic mean of three numbers");
+            Console.WriteLine("Introduce your first number:");
+
+            double firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce your second number:");
+
+            double secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce your third number:");
+
+            double thirdNumber = int.Parse(Console.ReadLine());
+
+            double arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+
+            Console.WriteLine("Arithmetic mean of the three numbers is: " + arithmeticMean);
+        }
+    }
+}
